dcf2edec0e4a41ae30c67f122e2a9494f15d63d4
Siddharth Sharma <65253410+03sid-sharma@users.noreply.github.com>
2024-01-04 16:46:48 +0530

    feat: highlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,31 +4,39 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props{
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectGenre}: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
-  if(error) return null;
-  
-  if(isLoading) return <Spinner/>;
+  if (error) return null;
+
+  if (isLoading) return <Spinner />;
 
   return (
-      <List>
-        {data.map((genre) => (
-          <ListItem key={genre.id} paddingY='5px'>
-            {" "}
-            <HStack>
-              <Image
-                boxSize="32px"
-                borderRadius={8}
-                src={getCroppedImageUrl(genre.image_background)}
-              />
-              <Button onClick={() => onSelectGenre(genre)} variant='link' fontSize='lg'>{genre.name}</Button>
-            </HStack>{" "}
-          </ListItem>
-        ))}
-      </List>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          {" "}
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Button
+              fontWeight={genre.id === selectedGenre?.id? 'bold': 'normal'}
+              onClick={() => onSelectGenre(genre)}
+              variant="link"
+              fontSize="lg"
+            >
+              {genre.name}
+            </Button>
+          </HStack>{" "}
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
