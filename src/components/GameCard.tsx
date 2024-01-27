cc452996bf1d6e49e979622b6f23d7ecc3c929cb
Siddharth Sharma <65253410+03sid-sharma@users.noreply.github.com>
2024-01-27 23:49:59 +0530

    refactor: cleanup the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,13 +13,13 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between" flexWrap="wrap">
+        <HStack marginBottom={3} justifyContent="space-between" flexWrap="wrap">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
