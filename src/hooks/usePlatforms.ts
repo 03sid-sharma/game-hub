c422838d6356e98ccda050e504bf05631214c3a0
Siddharth Sharma <65253410+03sid-sharma@users.noreply.github.com>
2024-01-29 09:26:26 +0530

    refactor: shipping static data

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,10 @@
-import useData from "./useData";
-
+import data from "../data/platforms";
 export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () => ({ data: data, isLoading: false, error: null });
 
 export default usePlatforms;
